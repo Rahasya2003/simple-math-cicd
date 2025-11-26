7b8ecf74cdde51d9c830c39e5b6a4b09e376093a
Rahasya2003 <gawanderahasya@gmail.com>
2025-11-26 17:01:49 +0530

    first commit

---
+++ src/math_ops.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+if __name__ == "__main__":
+    print("Additions : ",add(5,3))
+    print("Substractions : ",sub(5,3))
+    print("Multiplication : ",multiply(5,3))
+    
+    
